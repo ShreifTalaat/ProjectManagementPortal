9463073aa913dd71e1ce9df192f2ec20093830b2
Sherif Talaat <shreiftalaat14@gmail.com>
2024-05-01 16:01:03 +0300

    Project

---
+++ Controllers/ProjectController.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using BLL.Interfaces;
+using BLL.ModelViews;
+using DAL.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProjectController : ControllerBase
+    {
+        private readonly IProjectService _projectService;
+        private readonly IMapper _mapper;
+
+        public ProjectController(IProjectService projectService, IMapper mapper)
+        {
+            _projectService = projectService;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult GetProjects()
+        {
+            var projects = _projectService.GetProjects();
+            return Ok(_mapper.Map<List<ProjectMV>>(projects));
+        }
+
+        [HttpPost]
+        public IActionResult AddProject([FromBody] ProjectMV model)
+        {
+            var result = _projectService.AddProject(_mapper.Map<Project>(model));
+            return result > 0 ? Ok(result) : BadRequest("Failed to add project");
+        }
+    }
+}
---
+++ Controllers/TaskController.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using BLL.Interfaces;
+using BLL.ModelViews;
+using BLL.Services;
+using DAL.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TaskController : ControllerBase
+    {
+        private readonly ITask _taskService;
+        private readonly IMapper _mapper;
+
+        public TaskController(ITask taskService, IMapper mapper)
+        {
+            _taskService = taskService;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult GetTasks()
+        {
+            var tasks = _taskService.GetTasks();
+            return Ok(_mapper.Map<List<TaskMV>>(tasks));
+        }
+
+        [HttpPost]
+        public IActionResult AddTask([FromBody] TaskMV model)
+        {
+            var result = _taskService.AddTask(_mapper.Map<DAL.Models.Task>(model));
+            return result > 0 ? Ok(result) : BadRequest("Failed to add task");
+        }
+    }
+}
